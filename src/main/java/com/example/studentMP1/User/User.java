7165b59cd64b094d3cda781c99d40f23f715111a
sashik mindaka <mindakaofficial0069@gmail.com>
2026-04-23 00:35:07 +0530

    model class

--- src/main/java/com/example/studentMP1/User/User.java
+++ src/main/java/com/example/studentMP1/User/User.java
@@ -2,6 +2,8 @@ package com.example.studentMP1.User;
 
 import com.example.studentMP1.Model.Role;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,6 +14,8 @@ import java.util.List;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
 public class User implements UserDetails {
 
     @Id
@@ -39,5 +43,25 @@ public class User implements UserDetails {
     @Override
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
